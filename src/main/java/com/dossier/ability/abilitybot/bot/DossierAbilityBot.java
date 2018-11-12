ddbd008b841db360af6afdc8b05983663d01bf27
d35h <danielzaru@gmail.com>
2018-11-12 23:05:44 +0100

    Refactor some code

--- src/main/java/com/dossier/ability/abilitybot/bot/DossierAbilityBot.java
+++ src/main/java/com/dossier/ability/abilitybot/bot/DossierAbilityBot.java
@@ -20,18 +20,23 @@ import static com.dossier.ability.abilitybot.abilities.AbilityCreator.DOSSIER_YE
 @Service
 public class DossierAbilityBot extends AbilityBot {
 
+    private static final String NOT_FOUND_TRY_AGAIN_MESSAGE = "I did not find you, try again later...";
+
     private final SubjectService subjectService;
+    private final int creatorId;
 
     public DossierAbilityBot(@Value("${settings.bot.name}") String botUsername,
                              @Value("${settings.bot.token}") String botToken,
+                             @Value("${settings.bot.creatorId}") int creatorId,
                              SubjectService subjectService) {
         super(botToken, botUsername);
         this.subjectService = subjectService;
+        this.creatorId = creatorId;
     }
 
     @Override
     public int creatorId() {
-        return 0;
+        return creatorId;
     }
 
     public Ability sendKeyboardLayout() {
@@ -49,8 +54,16 @@ public class DossierAbilityBot extends AbilityBot {
     private void replyWhenSubjectFound(MessageContext context, SilentSender sender) {
         if (context.firstArg().matches(DOSSIER_YEAR_ID_FORMAT)) {
             Optional<Subject> foundSubject = subjectService.findById(context.firstArg());
-            sender.send(foundSubject.isPresent() ? "You are on the list: " + foundSubject.get().getPdfUri() : "I did not find you, try again later...", context.chatId());
+            sender.send(foundSubject.map(this::getSubjectFoundMessage).orElse(getNotFoundMessage()), context.chatId());
         }
+    }
+
+    private String getSubjectFoundMessage(Subject subject) {
+        return "You are on the list" + subject.getPdfUri();
+    }
+
+    private String getNotFoundMessage() {
+        return NOT_FOUND_TRY_AGAIN_MESSAGE;
     }
 
 }
