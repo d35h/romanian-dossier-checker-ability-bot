7164eb8536884e38d3c4d84e6c3a80d00b9bae2c
d35h <danielzaru@gmail.com>
2018-11-19 19:35:06 +0100

    Fix liquibase population problems

--- src/main/java/com/dossier/ability/abilitybot/domain/LastProcessingDate.java
+++ src/main/java/com/dossier/ability/abilitybot/domain/LastProcessingDate.java
@@ -17,7 +17,7 @@ import java.util.Date;
 public class LastProcessingDate {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "last_processing_date")
